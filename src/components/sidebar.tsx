4bdd07415f051564983773ddf5a65930cf6910d4
Owen Auch <owenauch@asana.com>
2020-10-12 23:25:12 -0700

    got partial structure laid out

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Sidebar: React.FC = () => (
+  <div className="Sidebar">
+    <div className="Sidebar-outlineContainer">
+      <p className="Sidebar-mainName">Owen Auch</p>
+    </div>
+  </div>
+)
+
+export default Sidebar
