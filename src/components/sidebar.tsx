dbbc86a492997cce95d8c931736066eeec6f9e5a
Owen Auch <owenauch@asana.com>
2020-10-12 23:48:14 -0700

    added hover idea

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,8 @@ const Sidebar: React.FC = () => (
   <div className="Sidebar">
     <div className="Sidebar-outlineContainer">
       <p className="Sidebar-mainName">Owen Auch</p>
+      <p>thing 1</p>
+      <p>thing 2</p>
     </div>
   </div>
 )
