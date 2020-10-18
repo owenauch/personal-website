7f33934c7f79376d11966046858efdb6844f1932
Owen Auch <owenauch@asana.com>
2020-10-18 11:24:37 -0700

    random photo works

--- src/components/big_picture.tsx
+++ src/components/big_picture.tsx
@@ -1,12 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
 
-const BigPicture: React.FC = () => (
-  <div className="BigPicture">
-    <img
-      className="BigPicture-image Global-bigBorder"
-      src="https://www.worldatlas.com/upload/70/32/73/shutterstock-310887800.jpg"
-    />
-  </div>
-)
+const NUM_PHOTOS = 34
+
+const BigPicture: React.FC = () => {
+  const randomPhotoName = Math.floor(Math.random() * NUM_PHOTOS)
+  const randomPhotoSrc = `https://owenauch-photos.s3-us-west-1.amazonaws.com/${randomPhotoName}.JPG`
+
+  return (
+    <div className="BigPicture">
+      <img className="BigPicture-image Global-bigBorder" src={randomPhotoSrc} />
+      <div className="BigPicture-explainerText">
+        I took this photo while travelling! Click <Link to="/">here</Link> or
+        refresh the page to see a new one.
+      </div>
+    </div>
+  )
+}
 
 export default BigPicture
