c8abf3be6f75226198612ee9006b0377ae356359
Owen Auch <owenauch@asana.com>
2020-10-18 11:36:29 -0700

    some sort of skeleton

---
+++ src/pages/inklings.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Sidebar from "../components/sidebar"
+
+const Inklings: React.FC = () => (
+  <div className="Global-mainContainer">
+    <Sidebar />
+  </div>
+)
+
+export default Inklings
