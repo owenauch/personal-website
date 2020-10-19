1ebe5c387e1b957bd26b51bc153f55c72a37009b
Owen Auch <owenauch@asana.com>
2020-10-18 22:25:25 -0700

    first draft of about

--- src/components/about_content.tsx
+++ src/components/about_content.tsx
@@ -1,12 +1,50 @@
 import React from "react"
 import aboutPhoto from "../images/me.jpg"
+import { Link } from "gatsby"
 
 const AboutContent: React.FC = () => (
   <div className="AboutContent Global-bigBorder">
-    <img
-      src={aboutPhoto}
-      className="AboutContent-aboutPhoto Global-bigBorder"
-    />
+    <div className="AboutContent-imageContainer">
+      <img
+        src={aboutPhoto}
+        className="AboutContent-aboutPhoto Global-bigBorder"
+      />
+    </div>
+    <div className="AboutContent-text">
+      <p>
+        I work as a software engineer at Asana in San Francisco. Previously, I
+        worked at a couple of small startups, and studied economics and computer
+        science at Washington University in St. Louis.
+      </p>
+      <p className="AboutContent-thingsILike">
+        Things I like (non-exhaustive):
+      </p>
+      <ul>
+        <li>Being outside</li>
+        <li>
+          <Link to="https://open.spotify.com/user/1222073?si=TRlnYRvkStab6VtJWN7J8w">
+            Music
+          </Link>{" "}
+          (preferably indie, funk, hip hop and jazz)
+        </li>
+        <li>
+          Building things (usually with{" "}
+          <Link to="https://github.com/owenauch">code</Link>)
+        </li>
+        <li>
+          <Link to="https://www.goodreads.com/review/list/27881811-owen?ref=nav_mybooks&shelf=read&sort=rating">
+            Reading
+          </Link>{" "}
+          and <Link to="/inklings/">writing</Link>
+        </li>
+        <li>Friendships</li>
+        <li>Theology</li>
+      </ul>
+      <p className="AboutContent-finalBlurb">
+        If you also like any of these things, feel free to reach out to me at{" "}
+        <Link to="mailto:owenauch@gmail.com">owenauch@gmail.com</Link>!
+      </p>
+    </div>
   </div>
 )
 
