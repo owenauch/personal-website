ebb05988b9e6fb892dbab7373248c65ccead6453
Owen Auch <owenauch@asana.com>
2020-10-18 22:33:15 -0700

    add github icon

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import SidebarLinkContainer from "../components/sidebar_link_container"
-import { FiTwitter, FiMail } from "react-icons/fi"
+import { FiTwitter, FiMail, FiGithub } from "react-icons/fi"
 import { FaLinkedinIn } from "react-icons/fa"
 
 const Sidebar: React.FC = () => (
@@ -12,18 +12,21 @@ const Sidebar: React.FC = () => (
       </Link>
       <SidebarLinkContainer isMobile={false} />
       <div className="Sidebar-iconContainer">
-        <a href="https://twitter.com/owenauch" className="Sidebar-icon">
+        <Link to="https://twitter.com/owenauch" className="Sidebar-icon">
           <FiTwitter color="black" style={{ verticalAlign: "middle" }} />
-        </a>
-        <a href="mailto:owenauch@gmail.com" className="Sidebar-icon">
+        </Link>
+        <Link to="mailto:owenauch@gmail.com" className="Sidebar-icon">
           <FiMail color="black" style={{ verticalAlign: "middle" }} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/owenauch/"
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/owenauch/"
           className="Sidebar-icon"
         >
           <FaLinkedinIn color="black" style={{ verticalAlign: "middle" }} />
-        </a>
+        </Link>
+        <Link to="https://github.com/owenauch" className="Sidebar-icon">
+          <FiGithub color="black" style={{ verticalAlign: "middle" }} />
+        </Link>
       </div>
     </div>
     <SidebarLinkContainer isMobile={true} />
