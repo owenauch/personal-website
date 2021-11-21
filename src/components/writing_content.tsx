dc6dc9b15cbd8dadf31383bdc8fe8f923869bba0
Owen Auch <owenauch@asana.com>
2021-11-21 07:07:15 -0600

    need to finish styling and add description to bottled rage

--- src/components/writing_content.tsx
+++ src/components/writing_content.tsx
@@ -9,20 +9,33 @@ interface Link {
   date: string
 }
 
+interface Section {
+  sectionName: string
+  links: Link[]
+}
+
 const WritingContent: React.FC = () => {
   const linkSortFunction = (link1: Link, link2: Link) =>
     Date.parse(link2.date) - Date.parse(link1.date)
-  const writingLinks = WritingDataJSON.links
-    .sort(linkSortFunction)
-    .map((link: Link) => (
-      <WritingLink
-        key={link.title}
-        title={link.title}
-        description={link.description}
-        link={link.link}
-        date={link.date}
-      />
-    ))
+  const writingLinks = WritingDataJSON.sections.map((section: Section) => {
+    const links = section.links
+      .sort(linkSortFunction)
+      .map((link: Link) => (
+        <WritingLink
+          key={link.title}
+          title={link.title}
+          description={link.description}
+          link={link.link}
+          date={link.date}
+        />
+      ))
+    return (
+      <div key={section.sectionName}>
+        <h2>{section.sectionName}</h2>
+        {links}
+      </div>
+    )
+  })
   return <div className="WritingContent Global-bigBorder">{writingLinks}</div>
 }
 
